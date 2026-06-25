3fdcebd4800c9ab03482e53379e8eca0394e780d
dorineachieng682-ship-it <dorineachieng682@gmail.com>
2026-06-25 17:07:51 +0300

    Added contact links and improved portfolio

--- script.js
+++ script.js
@@ -1,96 +1,13 @@
-// Fade-In Animation
+const themeBtn = document.getElementById("theme-toggle");
 
-const fadeElements = document.querySelectorAll(".fade-in");
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-fadeElements.forEach((element) => {
-    observer.observe(element);
-});
-
-
-// Dark Mode Toggle
-
-const themeToggle = document.getElementById("theme-toggle");
-
-// Load saved theme
-
-if(localStorage.getItem("theme") === "dark"){
-    document.body.classList.add("dark-mode");
-    themeToggle.textContent = "☀️";
-}
-
-// Toggle Theme
-
-themeToggle.addEventListener("click", () => {
-
+themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if(document.body.classList.contains("dark-mode")){
-
-        themeToggle.textContent = "☀️";
-
-        localStorage.setItem("theme","dark");
-
-    } else {
-
-        themeToggle.textContent = "🌙";
-
-        localStorage.setItem("theme","light");
+        themeBtn.textContent = "☀️";
+    }else{
+        themeBtn.textContent = "🌙";
     }
-});
-
-const text =
-"Frontend Developer | React Developer | Problem Solver";
-
-let i = 0;
-
-function typeWriter(){
-
-    if(i < text.length){
-
-        document.getElementById("typing-text").innerHTML += text.charAt(i);
-
-        i++;
-
-        setTimeout(typeWriter, 70);
-    }
-}
-
-typeWriter();
-
-
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".navbar a");
-
-window.addEventListener("scroll", () => {
-
-    let current = "";
-
-    sections.forEach(section => {
-
-        const sectionTop = section.offsetTop;
-
-        if(pageYOffset >= sectionTop - 150){
-            current = section.getAttribute("id");
-        }
-
-    });
-
-    navLinks.forEach(link => {
-
-        link.classList.remove("active");
-
-        if(link.getAttribute("href") === `#${current}`){
-            link.classList.add("active");
-        }
-    });
 });
 
 const topBtn = document.getElementById("topBtn");
@@ -99,18 +16,15 @@ window.addEventListener("scroll", () => {
 
     if(window.scrollY > 300){
         topBtn.style.display = "block";
-    } else {
+    }else{
         topBtn.style.display = "none";
     }
 
 });
 
 topBtn.addEventListener("click", () => {
-
     window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+        top:0,
+        behavior:"smooth"
     });
-
 });
-
